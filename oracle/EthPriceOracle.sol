9a91ebc4193ec58a87cbee8972da3d01d1a6dac5
winterbreaker <joejamesdev@hotmail.com>
2024-09-22 21:51:02 -0700

    Update EthPriceOracle.sol

--- oracle/EthPriceOracle.sol
+++ oracle/EthPriceOracle.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.5.0;
 import "openzeppelin-solidity/contracts/access/Roles.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./CallerContractInterface.sol";
 contract EthPriceOracle {
   using Roles for Roles.Role;
   Roles.Role private owners;
   Roles.Role private oracles;
+  using SafeMath for uint256;
   uint private randNonce = 0;
   uint private modulus = 1000;
   uint private numOracles = 0;
@@ -58,11 +60,17 @@ contract EthPriceOracle {
     requestIdToResponse[_id].push(resp);
     uint numResponses = requestIdToResponse[_id].length;
     if (numResponses == THRESHOLD) {
+      uint computedEthPrice = 0;
+        for (uint f=0; f < requestIdToResponse[_id].length; f++) {
+        computedEthPrice = computedEthPrice.add(requestIdToResponse[_id][f].ethPrice);
+      }
+      computedEthPrice = computedEthPrice.div(numResponses);
       delete pendingRequests[_id];
+      delete requestIdToResponse[_id];
       CallerContractInterface callerContractInstance;
       callerContractInstance = CallerContractInterface(_callerAddress);
-      callerContractInstance.callback(_ethPrice, _id);
-      emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
+      callerContractInstance.callback(computedEthPrice, _id); // Update this line code
+      emit SetLatestEthPriceEvent(computedEthPrice, _callerAddress); // Update this line of code
     }
   }
 }
